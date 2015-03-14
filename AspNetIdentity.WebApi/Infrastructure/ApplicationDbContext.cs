5897356be8aee3c1bd0972d3454ed80496e1d205
Keith Smith <keith@kssoftware.net>
2015-03-14 12:29:27 -0700

    Adding User Infrastructure

---
+++ AspNetIdentity.WebApi/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetIdentity.WebApi.Infrastructure
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext():base("DefaultConnectin", throwIfV1Schema: false)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
