ffba4bd363495377bfa7c57143c9eb4b3cc93715
Keith Smith <keith@kssoftware.net>
2015-03-14 14:31:26 -0700

    Add ApplicationUserManager

---
+++ AspNetIdentity.WebApi/Infrastructure/ApplicationUserManger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace AspNetIdentity.WebApi.Infrastructure
+{
+    public class ApplicationUserManger : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManger(IUserStore<ApplicationUser> store) : base(store)
+        {
+        }
+
+        public static ApplicationUserManger Create(IdentityFactoryOptions<ApplicationUserManger> options,
+            IOwinContext context)
+        {
+            var appDbContext = context.Get<ApplicationDbContext>();
+            var appUserManager = new ApplicationUserManger(new UserStore<ApplicationUser>(appDbContext));
+            return appUserManager;
+        }
+    }
+}
