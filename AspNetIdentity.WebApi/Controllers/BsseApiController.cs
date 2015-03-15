79d2abe66516a6917f96134eba3dc0689c9288b2
Keith Smith <keith@kssoftware.net>
2015-03-14 17:32:46 -0700

    Working Basic Account Controller

---
+++ AspNetIdentity.WebApi/Controllers/BsseApiController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using AspNetIdentity.WebApi.Infrastructure;
+using AspNetIdentity.WebApi.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace AspNetIdentity.WebApi.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        private ModelFactory _modelFactory;
+        private readonly ApplicationUserManger _appUserManger = null;
+
+        protected ApplicationUserManger AppUserManager
+        {
+            get { return _appUserManger ?? Request.GetOwinContext().GetUserManager<ApplicationUserManger>(); }
+        }
+
+        protected  ModelFactory TheModelFactory
+        {
+            get { return _modelFactory ?? (_modelFactory = new ModelFactory(this.Request, this.AppUserManager)); }
+        }
+
+        protected IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+                return InternalServerError();
+
+            if (result.Succeeded) return null;
+
+            if (result.Errors != null)
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError("", error);
+                
+            if (ModelState.IsValid)
+                return BadRequest();
+
+            return BadRequest(ModelState);
+        }
+    }
+}
